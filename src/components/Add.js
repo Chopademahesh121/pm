3a02007acd51c2b8e13318d45551f50b7ad4d52b
pranitmule1998 <pranitmule0@gmail.com>
2023-10-11 18:37:32 +0530

    hhh

--- src/components/Add.js
+++ src/components/Add.js
@@ -1,12 +1,8 @@
-const Add = () => {
-    return (
+const Add =()=>{
+    return(
         <>
-            <h1>I am add pranit </h1>
-            <h2>Student Form</h2>
-
-            <label htmlFor="name">Name</label>
-            <input type="text" id="name"></input>
-
+        <h1>I am add pranit </h1>
+        
         </>
     )
 }
