6a0b170096439b7cef12c98be78559b7e8fde9de
chopade121 <chopademahesh121@gmail.com>
2023-10-11 18:52:40 +0530

    hello

--- src/components/Add.js
+++ src/components/Add.js
@@ -1,9 +1,12 @@
-const Add =()=>{
-    return(
+const Add = () => {
+    return (
         <>
-        <h1>I am add pranit </h1>
-        <label>ok</label>
-        
+            <h1>I am add pranit </h1>
+            <label>ok</label>
+
+
+            <h2>Hello Pranit I am Sending pull request ok</h2>
+
         </>
     )
 }
