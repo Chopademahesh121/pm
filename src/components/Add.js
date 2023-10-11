40edccb3eda907fdb5a4334256554241acf20987
pranitmule1998 <pranitmule0@gmail.com>
2023-10-11 16:21:52 +0530

    changes

---
+++ src/components/Add.js
@@ -0,0 +1,8 @@
+const Add =()=>{
+    return(
+        <>
+        <h1>I am add</h1>
+        </>
+    )
+}
+export default Add;
---
+++ src/components/Show.js
@@ -0,0 +1,8 @@
+const Show =()=>{
+    return(
+        <>
+        <h1>I am show</h1>
+        </>
+    )
+}
+export default Show;
