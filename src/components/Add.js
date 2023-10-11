42863af90819dd71f3a8ff28d635602e20a5ff34
chopade121 <chopademahesh121@gmail.com>
2023-10-11 18:33:07 +0530

    hello

--- src/components/Add.js
+++ src/components/Add.js
@@ -2,6 +2,9 @@ const Add =()=>{
     return(
         <>
         <h1>I am add pranit </h1>
+        <h2>Student Form</h2>
+        
+        <input type="text" id="name"></input>
         </>
     )
 }
