c0c53431c5cfed25947a226b033cdd5c50b1ec18
chopade121 <chopademahesh121@gmail.com>
2023-10-11 18:30:47 +0530

    hello

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div>
+
+
+            <h1>Hello I am a Mahesh Navbar</h1>
+
+
+
+        </div>
+    )
+}
+
+export default Navbar
